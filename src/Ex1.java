006e61b91f511503aac43731f6e766b106a2d889
Noam <noamasher55@gmail.com>
2024-12-01 16:51:19 +0200

    Implement isNumber Method

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,2 +1,24 @@
 public class Ex1 {
+    public static boolean isNumber(String num) {
+        int index = num.indexOf('b');
+        if(index == -1) {
+            return false;
+        }
+        String sub = num.substring(index+1);
+        if(sub.length() >=2 ) {
+            return false;
+        }
+        char c = sub.charAt(0);
+        int base = (int)c;
+        if(!((base >= 50 && base <= 57) || (base >= 65 && base <= 70))){
+            return false;
+        }
+        sub = num.substring(0,index);
+        for (int i = 0; i < sub.length(); i++) {
+            if(!Character.isDigit(num.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
