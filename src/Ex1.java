8ad8f94fb545c6946c1257022e8680a679f743bd
Noam <noamasher55@gmail.com>
2024-12-03 22:36:21 +0200

    fixed isNumber method

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,32 +1,50 @@
 public class Ex1 {
+
     public static boolean isNumber(String num) {
         int index = num.indexOf('b');
         if(index == -1) {
-            for (int i = 0; i < num.length(); i++) {
-                if(!Character.isDigit(num.charAt(i))) {
-                    return false;
-                }
-            }
-            return true;
+            return allDigits(num);
         }
         String sub = num.substring(index+1);
         if(sub.length() != 1) {
+            System.out.println(1);
             return false;
         }
         char c = sub.charAt(0);
-        int base = (int)c;
-        if(!((base >= 50 && base <= 57) || (base >= 65 && base <= 70))){
+        int base =(int)c;
+        if(!((base >= 50 && base <= 57) || (base >= 65 && base <= 71))){
+            System.out.println(2);
             return false;
         }
+        base = getvalue(c);
         sub = num.substring(0,index);
         if(sub.isEmpty()){
+            System.out.println(3);
             return false;
         }
-        for (int i = 0; i < sub.length(); i++) {
-            if(!Character.isDigit(num.charAt(i))){
+        for(int i = 0; i < sub.length(); i++){
+            if(getvalue(sub.charAt(i)) >= base || getvalue(sub.charAt(i)) == -1 ){
+                System.out.println(4);
                 return false;
             }
         }
         return true;
+    }
+    public static boolean allDigits(String num) {
+        for (int i = 0; i < num.length(); i++) {
+            if(!Character.isDigit(num.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int getvalue(char c ){
+        char[] arr = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G'};
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == c){
+                return i;
+            }
+        }
+        return -1;
     }
 }
